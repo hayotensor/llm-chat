76d13e81b26b7cedf607742b8b0fb55ba2dcb31a
hayotensor <hayotensor@gmail.com>
2024-09-04 08:26:05 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+import hivemind
+from flask import Flask
+from flask_cors import CORS
+from flask_sock import Sock
+
+import utils
+import views
+
+logger = hivemind.get_logger(__file__)
+
+logger.info("Loading models")
+models = utils.load_models()
+
+logger.info("Starting Flask app")
+app = Flask(__name__)
+CORS(app)
+app.config["SOCK_SERVER_OPTIONS"] = {"ping_interval": 25}
+sock = Sock(app)
+
+logger.info("Pre-rendering index page")
+index_html = views.render_index(app)
+
+
+@app.route("/")
+def main_page():
+    return index_html
+
+
+import http_api
+import websocket_api
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
